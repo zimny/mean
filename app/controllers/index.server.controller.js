0bad8598332da16220fb77df72383879763a819e
Piotr Zimoch <Zimny@Piotrs-MacBook-Pro.local>
2015-08-08 17:35:25 +0200

    angular init

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,10 +1,6 @@
 exports.render = function(req, res) {
-	if(req.session.lastVisit){
-		console.log(req.session.lastVisit);
-	}
-	req.session.lastVisit = new Date();
-	
-	res.render('index', {
-		title: "Hello world"
-	})
+  res.render('index', {
+    title: 'Hello World',
+    userFullName: req.user ? req.user.fullName : ''
+  });
 };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,9 +1,18 @@
-var users = require("../../app/controllers/user.server.controller");
+var users = require('../../app/controllers/user.server.controller'),
+    passport = require('passport');
 
-module.exports = function(app){
-	app.route("/users").post(users.create).get(users.list);
-	app.route("/users/:userId").get(users.read).put(users.update).delete(users.delete);
+module.exports = function(app) {
+  app.route('/signup')
+     .get(users.renderSignup)
+     .post(users.signup);
 
-	app.param("userId", users.userByID);
-}
+  app.route('/signin')
+     .get(users.renderSignin)
+     .post(passport.authenticate('local', {
+       successRedirect: '/',
+       failureRedirect: '/signin',
+       failureFlash: true
+     }));
 
+  app.get('/signout', users.signout);
+};
